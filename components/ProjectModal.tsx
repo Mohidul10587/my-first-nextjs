b30e11e249a670cea780c757fb955d9fa039b8f0
mohid <mohid10587@gmail.com>
2026-07-10 01:09:50 +0600

    pk

--- components/ProjectModal.tsx
+++ components/ProjectModal.tsx
@@ -65,7 +65,7 @@ export default function ProjectModal({
         title,
         description,
         link,
-        image: imageUrl,
+        img: imageUrl,
       };
 
       const response = await fetch(
